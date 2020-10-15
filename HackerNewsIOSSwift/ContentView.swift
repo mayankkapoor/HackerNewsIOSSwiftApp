5d876b61338fbf79fb48c3810009964e759dfede
Mayank Kapoor <mayankkapoormail@gmail.com>
2020-10-15 15:36:21 +0530

    Initial Commit

---
+++ HackerNewsIOSSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNewsIOSSwift
+//
+//  Created by Mayank Kapoor on 15/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNewsIOSSwift/HackerNewsIOSSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsIOSSwiftApp.swift
+//  HackerNewsIOSSwift
+//
+//  Created by Mayank Kapoor on 15/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsIOSSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
